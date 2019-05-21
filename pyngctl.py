409a50bf53d914b8d57b7ffb64ffbb1b20b60f74
davejbarnes <davejbarnes@gmail.com>
2019-05-22 00:25:57 +0100

    Added find_mode()

--- pyngctl.py
+++ pyngctl.py
@@ -12,3 +12,12 @@ def timestamp(timestring: str = "now") -> int:
     shell_output = subprocess.check_output(['date', '-d ' + timestring, '+%s'], stderr=subprocess.STDOUT)[0:-1]
     return int(shell_output)
 
+def find_mode() -> str:
+    """Figure out which mode has been specified"""
+    modes = ["down", "ack", "dn", "en", "dc", "ec"]
+    for arg in args.validargs:
+        if str(arg) in modes:
+            return arg
+    # default to down mode
+    return "down"
+
